73c92cdc2ef98a9b72911b865c130e7ebb357570
Ihor Horobets <ihor@horobets.me>
2020-02-04 22:10:09 +0200

    Add exchange handler.

--- src/api.py
+++ src/api.py
@@ -1,5 +1,7 @@
 import requests
 
+import symbols
+
 
 def get_rates_data(currency="USD"):
     data = requests.get(
@@ -8,16 +10,20 @@ def get_rates_data(currency="USD"):
     return data["rates"]
 
 
-def get_exchange_data(out="USD", to="GBP", amount=1):
-    out = check_if_symbol(out)
-    to = check_if_symbol(to)
+def _check_if_symbol(currency):
+    return symbols.symbols.get(currency, currency)
+
+
+def get_exchange_data(out, to):
+    out = _check_if_symbol(out)
+    to = _check_if_symbol(to)
     response = requests.get(
         f"https://api.exchangeratesapi.io/latest?symbols={to}&base={out}"
     )
-    if response.get("error", None):
-        return response["error"]
-    response = amount * response["rates"][to]
-    return f"{amount} {out} equals {response:.2f} {to}"
+
+    if response.status_code != requests.codes.ok:
+        response.raise_for_status()
+    return response.json()["rates"]
 
 
 def format_date(date):
--- src/bot.py
+++ src/bot.py
@@ -34,25 +34,29 @@ Use `/history CURRENCY_1/CURRENCY_2 7 days` to get graph of currencies relation 
 
 
 @typing
-def get_list(update, context):
+def get_list_handler(update, context):
     currency = "USD"
-    previous_rates = rates_collection.find_one({"base": currency})
-    now = datetime.now()
-    if not previous_rates.get("date"):
-        previous_rates["date"] = now
-    if not previous_rates or (now + timedelta(minutes=10) >= previous_rates["date"]):
-        rates = api.get_rates_data(currency)
-        data = {"rates": rates, "date": datetime.now()}
-        rates_collection.find_one_and_update(
-            query={"base": currency}, update={"$set": data}, upsert=True
-        )
-    else:
-        rates = previous_rates["rates"]
+    response = 'Some error happened. Sorry for inconvenience.'
+    try:
+        previous_rates = rates_collection.find_one({"base": currency})
+        now = datetime.now()
+        if not previous_rates.get("date"):
+            previous_rates["date"] = now
+        if not previous_rates or (now + timedelta(minutes=10) >= previous_rates["date"]):
+            rates = api.get_rates_data(currency)
+            data = {"rates": rates, "date": datetime.now()}
+            rates_collection.find_one_and_update(
+                query={"base": currency}, update={"$set": data}, upsert=True
+            )
+        else:
+            rates = previous_rates["rates"]
 
-    response = "```\n"
-    for (currency, price) in rates.items():
-        response += f"{currency}: {price:.2f}\n"
-    response += "\n```"
+        response = "```\n"
+        for (currency, price) in rates.items():
+            response += f"{currency}: {price:.2f}\n"
+        response += "\n```"
+    except Exception as e:
+        logger.exception(e)
 
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=response, parse_mode=ParseMode.MARKDOWN
@@ -67,11 +71,50 @@ def start_handler(update, context):
     )
 
 
+@typing
+def exchange_handler(update, context):
+    args = context.args
+    if 'to' in args:
+        args.remove('to')
+    rates = None
+    out = "USD"
+    to = "GBP"
+    amount = 0
+    response = 'Some error happened. Sorry for inconvenience.'
+    if len(args) == 3:
+        amount, out, to = args
+        amount = float(amount)
+        out = out.upper()
+        to = to.upper()
+    elif len(args) == 2:
+        # could fail if input is something like USD10
+        out = args[0][0]  # first char of '$10' string
+        amount = float(args[0][1:])
+        to = args[-1].upper()
+    else:
+        response = 'Please, use `/exchange AMOUNT CURRENCY_1 CURRENCY_2` format.'
+    try:
+        rates = api.get_exchange_data(out, to)
+    except Exception as e:
+        logger.exception(e)
+    if rates:
+        total = amount * rates[to]
+        response = f"{amount} {out} equals {total:.2f} {to}"
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=response,
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
 if __name__ == "__main__":
     TOKEN = os.getenv("TOKEN")
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start_handler))
-    dispatcher.add_handler(CommandHandler("list", get_list))
+    dispatcher.add_handler(CommandHandler("list", get_list_handler))
+    dispatcher.add_handler(CommandHandler("lst", get_list_handler))
+    dispatcher.add_handler(CommandHandler('help', start_handler))
+    dispatcher.add_handler(CommandHandler('exchange', exchange_handler))
     updater.start_polling()
     updater.idle()
